9bb51b4249d6219ce09868bc14e7575a2aca7bcb
paglobal <jackamoah.pa@gmail.com>
2023-10-14 01:23:43 +0000

    chore: build site into docs folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: "docs",
   integrations: [
     starlight({
       title: "PromethiumJS",
