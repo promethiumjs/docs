1656b4ae974d50fb05ebd6a5195dac5b32472187
paglobal <jackamoah.pa@gmail.com>
2023-10-14 01:00:48 +0000

    chore: bootstrapping the docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,118 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "PromethiumJS",
+      social: {
+        github: "https://github.com/promethiumjs/promethium",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            { label: "Introduction", link: "/getting-started/introduction/" },
+            { label: "Installation", link: "/getting-started/installation/" },
+            {
+              label: "Project Environment Setup",
+              link: "/getting-started/project-environment-setup/",
+            },
+          ],
+        },
+        {
+          label: "Tutorial",
+          items: [
+            { label: "Tic-Tac-Toe", link: "/tutorial/tic-tac-toe/" },
+            { label: "Blog", link: "/tutorial/blog/" },
+            {
+              label: "Random Colour Generator",
+              link: "/tutorial/random-colour-generator/",
+            },
+          ],
+        },
+        {
+          label: "Core Concepts",
+          items: [
+            {
+              label: "PromethiumJS Components",
+              link: "/core-concepts/promethiumjs-components/",
+            },
+            { label: "State", link: "/core-concepts/state/" },
+            { label: "Memos", link: "/core-concepts/memos/" },
+            { label: "Effects", link: "/core-concepts/effects/" },
+            {
+              label: "Advanced State Management",
+              link: "/core-concepts/advanced-state-management/",
+            },
+            {
+              label: "Routing",
+              link: "/core-concepts/routing/",
+            },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            {
+              label: "UI Component Libraries",
+              link: "/guides/ui-component-libraries/",
+            },
+            {
+              label: "Ardenide",
+              link: "/guides/ardenide/",
+            },
+            {
+              label: "Tailwind",
+              link: "/guides/tailwind/",
+            },
+            {
+              label: "Astro",
+              link: "/guides/astro/",
+            },
+            {
+              label: "PromethiumJS DevTools",
+              link: "/guides/promethiumjs-devtools/",
+            },
+            {
+              label: "Server-Side Rendering",
+              link: "/guides/server-side-rendering/",
+            },
+            {
+              label: "Testing",
+              link: "/guides/testing/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [
+            { label: "h", link: "/reference/h/" },
+            { label: "adaptState", link: "/reference/adapt-state/" },
+            { label: "adaptEffect", link: "/reference/adapt-effect/" },
+            {
+              label: "adaptRenderEffect",
+              link: "/reference/adapt-render-effect/",
+            },
+            { label: "adaptSyncEffect", link: "/reference/adapt-sync-effect/" },
+            { label: "ParticleEntity", link: "/reference/particle-entity/" },
+            {
+              label: "DerivativeEntity",
+              link: "/reference/derivative-entity/",
+            },
+            { label: "ActionEntity", link: "/reference/action-entity/" },
+            { label: "Router", link: "/reference/router/" },
+            {
+              label: "TypeScript Utility Types",
+              link: "/reference/typescript-utility-types/",
+            },
+          ],
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,32 @@
-const colors = require('tailwindcss/colors');
-const starlightPlugin = require('@astrojs/starlight-tailwind');
+const starlightPlugin = require("@astrojs/starlight-tailwind");
+
+const accent = {
+  200: "#9BCDE0",
+  600: "#0876A0",
+  900: "#003B3B",
+  950: "#002a2b",
+};
+const gray = {
+  100: "#f5f6f9",
+  200: "#ebeef3",
+  300: "#bfc2c9",
+  400: "#878b98",
+  500: "#545864",
+  700: "#343843",
+  800: "#232631",
+  900: "#16181d",
+};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        accent,
+        gray,
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
 };
