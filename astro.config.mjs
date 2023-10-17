6b355d286783e80bffe19d0c1002c335635e01fb
paglobal <jackamoah.pa@gmail.com>
2023-10-17 13:07:30 +0000

    fix: remove base setting in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://promethiumjs.github.io",
-  base: "/docs",
   integrations: [
     starlight({
       title: "PromethiumJS",
